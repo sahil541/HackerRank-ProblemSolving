c956d795936aaf1df187dbed0c4abb262f3e5670
sahil541 <sahilkaif541@gmail.com>
2021-09-15 15:13:57 +0530

    Added camelCase problem solution.

--- camelCase.cpp
+++ camelCase.cpp
@@ -1,12 +1,12 @@
 int camelcase(string s) {
     int len = s.length();
     if(len == 1) return len;
-    int count = 1;
+    int ans = 1;
     for(int i=0;i<len;i++){
         if(isupper(s[i]))
         {
-            count++;
+            ans++;
         }
     }
-    return count;
+    return ans;
 }
